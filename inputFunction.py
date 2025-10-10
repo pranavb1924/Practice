ad85dfde0d9cbf67b0e90a5705777056c6cecf15
Pranav B <bhoopal.p@northeastern.edu>
2025-10-10 15:42:17 -0400

    Input function in python

---
+++ inputFunction.py
@@ -0,0 +1,8 @@
+print("WELCOME TO PYTHON PRACTICE!")
+
+
+name = input("PLEASE ENTER YOUR NAME: ")
+country = input ("PLEASE ENTER YOUR COUNTRY: ")
+
+
+print(f"Hello {name}! From {country} nice to meet you!")
