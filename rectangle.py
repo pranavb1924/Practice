082708c625a9b24b9ab4525e3c527fa9b82b073d
Pranav B <bhoopal.p@northeastern.edu>
2025-10-10 15:46:08 -0400

    Area of rectangle

---
+++ rectangle.py
@@ -0,0 +1,9 @@
+
+l = input("Please Enter The Length Of The Rectangle: ")
+w = input("Please Enter The Width Of The Rectangle: ")
+
+print(f"AREA OF RECTANGLE = LENGTH X WIDTH")
+
+area = float(l) * float(w)
+
+print(f"AREA OF RECTANGLE WITH LENGTH: {l} and WIDTH{w} is {area}")
